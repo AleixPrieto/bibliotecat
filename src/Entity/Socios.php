daeb33303d934e989e0bccd4b3367014ecf4583f
Aleix Prieto <aleix3d@gmail.com>
2021-05-08 09:30:30 +0200

    taula socis

---
+++ src/Entity/Socios.php
@@ -0,0 +1,371 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SociosRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=SociosRepository::class)
+ */
+class Socios implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $dni;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $apellidos;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $nacimiento;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $direccion;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $poblacion;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $cp;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $provincia;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $telefono;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $alta;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    
+
+    /**
+     * Get the value of dni
+     */ 
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    /**
+     * Set the value of dni
+     *
+     * @return  self
+     */ 
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellidos
+     */ 
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set the value of apellidos
+     *
+     * @return  self
+     */ 
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nacimiento
+     */ 
+    public function getNacimiento()
+    {
+        return $this->nacimiento;
+    }
+
+    /**
+     * Set the value of nacimiento
+     *
+     * @return  self
+     */ 
+    public function setNacimiento($nacimiento)
+    {
+        $this->nacimiento = $nacimiento;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of direccion
+     */ 
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * Set the value of direccion
+     *
+     * @return  self
+     */ 
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of poblacion
+     */ 
+    public function getPoblacion()
+    {
+        return $this->poblacion;
+    }
+
+    /**
+     * Set the value of poblacion
+     *
+     * @return  self
+     */ 
+    public function setPoblacion($poblacion)
+    {
+        $this->poblacion = $poblacion;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cp
+     */ 
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * Set the value of cp
+     *
+     * @return  self
+     */ 
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of provincia
+     */ 
+    public function getProvincia()
+    {
+        return $this->provincia;
+    }
+
+    /**
+     * Set the value of provincia
+     *
+     * @return  self
+     */ 
+    public function setProvincia($provincia)
+    {
+        $this->provincia = $provincia;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of telefono
+     */ 
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    /**
+     * Set the value of telefono
+     *
+     * @return  self
+     */ 
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of alta
+     */ 
+    public function getAlta()
+    {
+        return $this->alta;
+    }
+
+    /**
+     * Set the value of alta
+     *
+     * @return  self
+     */ 
+    public function setAlta($alta)
+    {
+        $this->alta = $alta;
+
+        return $this;
+    }
+}
